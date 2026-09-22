7d623c40494c6e55388e32fe22880f61ee66e196
MoreWayPixel <mhrizky017@gmail.com>
2026-09-22 12:04:14 +0800

    second commit

---
+++ Kelas/main.py
@@ -0,0 +1,64 @@
+nama = "Rendy "
+Nama = "Ilham"
+hobi = "mancing"
+teks = "Rahmat Flowchart"
+Hujan = False
+Panas = True
+_nama_panjang = "Ahmad Rafi"
+
+angka_int1 = 10
+angka_int2 = 12
+a = 1.53
+b = 7
+c = a + b
+
+print(nama)
+print(_nama_panjang)
+print(Nama)
+print(angka_int1 + angka_int2)
+print(c)
+print(nama+hobi)
+
+print('''Halo saya Rendy
+Saya suka belajar coding
+Hari ini saya belajar bahasa pemrograman Python''')
+
+print(teks[3])
+print(teks[0:6])
+
+print(Hujan)
+
+data = ["Rafi", 10, True]
+print(data[1])
+print(data[1:3])
+
+data_mahasiswa = ("Rafi","034",True, 3.5)
+print(data_mahasiswa[0])
+print(data_mahasiswa[2])
+
+contoh_set = {1,2,2,2,3}
+print(contoh_set)
+
+mahasiswa = {
+    "nim":"12345",
+    "nama":"haikal",
+    "IPK":3.78
+}
+
+print(mahasiswa["nama"])
+
+# angka = 1
+# print(type(angka))
+
+# angka = str(angka)
+# print(type(angka))
+
+# nama = input("masukann nama anda: ")
+# print(nama)
+
+a = True
+b = False
+c = a and b
+d = a or b
+print(c)
+print(d)
